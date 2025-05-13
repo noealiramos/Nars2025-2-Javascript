8f2ea6cfe7e82f5b40e2cdf31d8376ae93a168ff
Noe Ali Ramos Saldivar <noeescorp@gmail.com>
2025-05-12 22:00:14 -0600

    Mis últimos cambios del 12may25 a las 10pm

--- semana2/clase1_semana2_ejercicioTarea.js
+++ semana2/clase1_semana2_ejercicioTarea.js
@@ -4,11 +4,18 @@ const {ask} = require('../helpers/input');
 
 async function main(){
 
-    for (let i=2; i<=100; i=i+2){
+  const numeroPar = Number(await ask("¿Hasta qué numero par quieres contar?: "));  
+  console.log("Va Ascendente con FOR:");  
+  for (let i=2; i<=numeroPar; i=i+2){
     console.log(i);
-
   }
 
+console.log("---------------");  
+
+  //console.log("Va Descendente con FOR: PENDINTE POR TERMINAR");  
+  //for (let i=numeroPar; i<=numeroPar; i=i+2){
+  //  console.log(i);
+ }//
 
 }
 main();
